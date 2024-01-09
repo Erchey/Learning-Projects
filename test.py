bb08f877db57bc190390102973f42c412e34a425
Erchey <okpalakingsleyeche@gmail.com>
2024-01-09 09:56:52 +0100

    new file:   quiz-game-start.zip 	new file:   quiz-game-start/__pycache__/data.cpython-312.pyc 	modified:   test.py

--- test.py
+++ test.py
@@ -1,37 +1,15 @@
-import os
-
-# Clear the terminal screen based on the operating system
-def clear_screen():
-    # For Windows
-    if os.name == 'nt':
-        _ = os.system('cls')
-    # For Mac and Linux (here, 'posix' refers to Unix/Linux based systems)
-    # else:
-    #     _ = os.system('clear')
-
-bids = {}
-print('Welcome to the secret auction program.')
-condition = True
-
-def auction(bids):
-    highest_price = 0
-    highest_bidder = ''
-    for people in bids:
-        actual_bid = bids[people]
-        if actual_bid > highest_price:
-            highest_price = actual_bid
-            highest_bidder = people
-    print(f'The highest bidder is {highest_bidder} with ${highest_price}')
+class User():
     
+    def __init__(self) -> None:
+        print("new user created")
 
-while condition == True:
-    name =  input('What is your name?:\n')
-    user_bid = int(input('What\'s your bid?: $\n'))
-    other_players = input("Are there other bidders? Type 'yes' or 'no'\n").lower()
-    bids[name] = user_bid
+user_1 = User()
+user_1.id = "001"
+user_1.username = 'angela'
 
-    if other_players == 'yes':
-            clear_screen()
-    else:
-        condition = False
-        auction(bids)
+print(user_1.username)
+
+user_2 = User()
+user_2.username = 'Stone heart'
+
+print(user_2.username)
