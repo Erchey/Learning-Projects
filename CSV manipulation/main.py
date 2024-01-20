ce13ed1b76bada43854a289cfc45f68634616b9f
Erchey <okpalakingsleyeche@gmail.com>
2024-01-20 23:49:05 +0100

    line 49 modified

---
+++ CSV manipulation/main.py
@@ -0,0 +1,11 @@
+import csv
+import pandas
+
+
+
+data = pandas.read_csv("weather_data.csv")
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     for row in data:
+#         print(row)
---
+++ CSV manipulation/pandas_practice.py
@@ -0,0 +1,15 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+gray_squirrels = len(data[data['Primary Fur Color'] == 'Gray'])
+red_squirrels = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+black_squirrels = len(data[data['Primary Fur Color'] == 'Black'])
+print()
+
+data_dict = {
+    'Fur Color': ['Gray', 'Red', 'Black'],
+    'Count': [gray_squirrels, red_squirrels, black_squirrels]
+}
+dataframe = pandas.DataFrame(data_dict)
+print(dataframe.to_csv())
