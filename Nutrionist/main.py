831311513ed3dd62113ea05d3cd37f3d938721d2
Erchey <okpalakingsleyeche@gmail.com>
2024-02-02 22:16:35 +0100

    line 49 modified

--- Nutrionist/main.py
+++ Nutrionist/main.py
@@ -1,7 +1,10 @@
 import requests
+from datetime import datetime
+import os
 
-APP_ID = 'd0b75dcd'
-API_KEY = 'ffd9dd13a5f056f72685a17476f554ba'
+APP_ID = os.getenv('APP_ID')
+API_KEY = os.getenv('API_KEY')
+sheet_endpoint = os.getenv('SHEET_ENDPOINT')
 
 domain = 'https://trackapi.nutritionix.com'
 end_point = '/v2/natural/exercise'
@@ -17,4 +20,23 @@ headers = {
     "x-app-key": API_KEY,
 }
 response = requests.post(url=f'{domain}{end_point}', json=user_params, headers=headers)
-print(response.text)
+result = response.json()
+print(result)
+print(APP_ID)
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
