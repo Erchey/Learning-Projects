be1b49bd67f035ccdbe6f6a41b85c97e53c9eba7
Erchey <okpalakingsleyeche@gmail.com>
2024-02-09 14:28:08 +0100

    line 49 modified

--- Scrape Billboard/main.py
+++ Scrape Billboard/main.py
@@ -15,12 +15,13 @@ APP_ID = os.getenv('SPOTIPY_ID')
 print(APP_ID)
 
 
+
 # Spotipy
 # Initialize spotipy with OAuth authentication
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
-        redirect_uri="http://spotify.com",
+        redirect_uri="https://example.com",
         client_id="9832b65773484c2bb6e505712cd55b6e",
         client_secret="366b18ea6f34488d903da6fb2ba2f712",
         show_dialog=True,
@@ -33,12 +34,14 @@ sp = spotipy.Spotify(
 user_id = sp.current_user()["id"]
 
 # Create a new playlist
-playlist_name = "My New Playlist"
-playlist = sp.user_playlist_create(user_id, playlist_name, public=False)
+# playlist_name = "My New Playlist"
+# playlist = sp.playlist_add_items(user_id, playlist_name)
+
+# playlist = sp.user_playlists(user_id)
+playlist_id = '6i7wjZbDcrPCSGKoFZGnLx'
 
 # Add tracks to the playlist
 track_uris = [
-    "spotify:track:7qiZfU4dY1lWllzX7mPBI3",  # Replace with your track URIs
-    "spotify:track:2TOzTqQXNmR2zDJXihjZ2e"  # Replace with your track URIs
+    "spotify:track:2E4INiVkDRBXXkFL4kB1Vk"  # Replace with your track URIs
 ]
-sp.playlist_add_items(playlist["id"], track_uris)
+sp.playlist_add_items(playlist_id, track_uris)
--- Scrape Billboard/test.py
+++ Scrape Billboard/test.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        scope="playlist-modify-private",
+        scope="playlist-read-private",
         redirect_uri="https://example.com",
         client_id="9832b65773484c2bb6e505712cd55b6e",
         client_secret="366b18ea6f34488d903da6fb2ba2f712",
@@ -18,10 +18,23 @@ sp = spotipy.Spotify(
 user_id = sp.current_user()["id"]
 
 
-track_results = sp.search(q="artist:Olivia Rodrigo track:Enough for you", type="track", limit=1)
+track_results = sp.search(q="artist:baby keem track:highway 95", type="track", limit=1)
 print(track_results)
 if track_results["tracks"]["items"]:
     track_uri = track_results["tracks"]["items"][0]["uri"]
     print(f"Track URI: {track_uri}")
 else:
     print("No track found")
+
+
+
+# # Retrieve the user's playlists
+# playlists = sp.current_user_playlists()
+#
+# # Iterate through the playlists to find the desired playlist
+# for playlist in playlists['items']:
+#     if playlist['name'] == 'My New Playlist':  # Replace with your playlist name
+#         playlist_id = playlist['id']
+#         break
+#
+# print("Playlist ID:", playlist_id)
