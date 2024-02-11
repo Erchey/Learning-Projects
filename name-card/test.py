70bcb103cd912531214b86742604f9daa30a0be2
Erchey <okpalakingsleyeche@gmail.com>
2024-02-12 00:24:05 +0100

    line 49 modified

---
+++ name-card/test.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/bye')
+def bye():
+    return 'Bye!'
+
+@app.route('/<name>')
+def greet(name):
+    return f'Hello {name + 12}!'
+
+
+app.run(debug=True)
