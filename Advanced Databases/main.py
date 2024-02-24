bbccfce70a2f720bf2e2cb439076161fed2e4762
Erchey <okpalakingsleyeche@gmail.com>
2024-02-24 22:42:01 +0100

    modified:   Advanced Databases/books-collection.db 	modified:   Advanced Databases/main.py 	new file:   CSV manipulation/__pycache__/pandas.cpython-312.pyc 	modified:   Pong/__pycache__/ball.cpython-312.pyc 	deleted:    __pycache__/test.cpython-311.pyc 	modified:   cookie clicker.py 	new file:   quizzler-app-start/__pycache__/data.cpython-312.pyc 	new file:   quizzler-app-start/__pycache__/question_model.cpython-312.pyc 	new file:   quizzler-app-start/__pycache__/quiz_brain.cpython-312.pyc 	new file:   quizzler-app-start/__pycache__/ui.cpython-312.pyc 	new file:   salaries_by_college_major.csv

---
+++ Advanced Databases/main.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+db = sqlite3.connect('books-collection.db')
+cursor = db.cursor()
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='good_books'")
+
+cursor.execute("INSERT INTO books VALUES(2, 'Harry Potter', 'J. K. Rowling', '9.3')")
+db.commit()
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(new_book)
+        return redirect(url_for('add'))
+
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
