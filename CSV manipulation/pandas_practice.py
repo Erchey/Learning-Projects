a9de5db2f7ec2f7d241b7c5aad9af37c48f24c5c
Erchey <okpalakingsleyeche@gmail.com>
2024-02-26 21:25:34 +0100

    modified:   CSV manipulation/pandas_practice.py

--- CSV manipulation/pandas_practice.py
+++ CSV manipulation/pandas_practice.py
@@ -1,15 +1,6 @@
 import pandas
 
-data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-
-gray_squirrels = len(data[data['Primary Fur Color'] == 'Gray'])
-red_squirrels = len(data[data['Primary Fur Color'] == 'Cinnamon'])
-black_squirrels = len(data[data['Primary Fur Color'] == 'Black'])
-print()
-
-data_dict = {
-    'Fur Color': ['Gray', 'Red', 'Black'],
-    'Count': [gray_squirrels, red_squirrels, black_squirrels]
-}
-dataframe = pandas.DataFrame(data_dict)
-print(dataframe)
+data = pandas.read_csv("weather_data.csv")
+max_temp = data.temp == data.temp.max()
+data_dict = data.to_dict()
+print(data[max_temp])
